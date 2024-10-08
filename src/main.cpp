c358fc0e29645ca554557669a9f239222ea6d07a
jrrlucenafilho <jrrlucenafilho@gmail.com>
2024-10-08 12:24:09 -0300

    fix: tweaked 2-opt fee comparison. Removed Disturbance comment suggestion about disturbing curr_best_iter

--- src/main.cpp
+++ src/main.cpp
@@ -102,6 +102,9 @@ bool BestImprovement2Opt(Instance &instance, Solution &solution) {
       if (new_cost < old_cost) {
         old_cost = new_cost;
         best_sequence = new_sequence;
+      } else {
+        // So i always compare it to the input solution, and not last iter's 2-opt'ed solution
+        new_sequence = solution.getSolution();
       }
     }
   }
@@ -235,8 +238,6 @@ Solution IteratedLocalSearch(int max_iters, int max_iters_ILS,
       }
 
             // Disturbance to help solution not fall into a local best pitfall
-            // Preferably disturb the curr_best_solution, disturbing from curr_iter_solution causes fluctuations in the final solution
-            // on big and heavy instances (gets always close to optimal answer, but never quite so)
             curr_iter_solution = Disturbance(instance, curr_iter_solution);
             curr_iter_counter_ILS++;
         }
