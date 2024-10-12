8c6f79525ebdb670df6e5c7dcdd80bd5f0c62da0
jrrlucenafilho <jrrlucenafilho@gmail.com>
2024-10-12 16:48:57 -0300

    feat: add iter count on benchmarker

--- src/headers/Benchmarker.hpp
+++ src/headers/Benchmarker.hpp
@@ -6,10 +6,12 @@
 #include <vector>
 
 struct benchmarker_t {
-    // Solution info
+    // Solution info (will act as a sum and then be divided by 10 on print)
     int32_t constructive_heuristic_avg_cost = 0;
     int32_t rvnd_avg_cost = 0;
     int32_t metaheuristic_solution_avg_cost = 0;
+
+    int32_t best_solution_iter;
 
     std::vector<size_t> best_solution_sequence;
     int32_t best_solution_cost;
@@ -30,25 +32,29 @@ struct benchmarker_t {
         std::cout << "Instance: " << instance_name << '\n';
         std::cout << "Optimal Cost: " << optimal_cost << '\n';
         std::cout << "--------------------------Constructive Heuristic-----------------------------\n";
-        std::cout << "Constructive Heuristic Avg Cost: " << constructive_heuristic_avg_cost << '\n';
-        std::cout << "Constructive Heuristic Avg Time: " << constructive_heuristic_avg_elapsed_time.count() << '\n';
+        std::cout << "Constructive Heuristic Avg Cost: " << constructive_heuristic_avg_cost / 10.0 << '\n';
+        std::cout << "Constructive Heuristic Avg Time: " << constructive_heuristic_avg_elapsed_time.count() / 10.0 << '\n';
         std::cout << "Constructive Heuristic GAP" << CalculateGap(constructive_heuristic_avg_cost, optimal_cost) << '\n';
         std::cout << "----------------------------------RVND---------------------------------------\n";
-        std::cout << "RVND Avg Cost: " << rvnd_avg_cost << '\n';
-        std::cout << "RVND Avg Time: " << rvnd_avg_elapsed_time.count() << '\n';
+        std::cout << "RVND Avg Cost: " << rvnd_avg_cost / 10.0 << '\n';
+        std::cout << "RVND Avg Time: " << rvnd_avg_elapsed_time.count() / 10.0 << '\n';
         std::cout << "RVND GAP: " << CalculateGap(rvnd_avg_cost, optimal_cost);
         std::cout << "-----------------------------Metaheuristic-----------------------------------\n";
-        std::cout << "Metaheuristic Avg Cost: " << metaheuristic_solution_avg_cost << '\n';
-        std::cout << "Metahueristic Avg Time: " << metaheuristic_avg_elapsed_time.count() << " s" << '\n';
+        std::cout << "Metaheuristic Avg Cost: " << metaheuristic_solution_avg_cost / 10.0 << '\n';
+        std::cout << "Metahueristic Avg Time: " << metaheuristic_avg_elapsed_time.count() / 10.0 << " s" << '\n';
         std::cout << "Metaheuristic GAP: " << CalculateGap(metaheuristic_solution_avg_cost, optimal_cost) << '\n';
         std::cout << "--------------------------Best Solution Found--------------------------------\n";
         std::cout << "Best Solution Cost: " << best_solution_cost << '\n';
+        std::cout << "Best Solution Iteration: " << best_solution_iter << '\n';
         std::cout << "Best Solution Sequence: ";
 
-        for (size_t element : best_solution_sequence) {
-            std::cout << element << ", ";
-        }
+        for (size_t i = 0; i < best_solution_sequence.size(); i++) {
+            std::cout << best_solution_sequence[i];
 
+            if (i != best_solution_sequence.size() - 1) {
+                std::cout << ", ";
+            }
+        }
         std::cout << '\n';
     }
 };
