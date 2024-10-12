3692a2208077b95d04ebbf13214cce0f16e6555b
jrrlucenafilho <jrrlucenafilho@gmail.com>
2024-10-12 16:30:56 -0300

    feat: add printResults() to benhcmarker

--- src/headers/Benchmarker.hpp
+++ src/headers/Benchmarker.hpp
@@ -7,32 +7,42 @@
 
 struct benchmarker_t {
     // Solution info
-    int32_t constructive_heuristic_avg_cost;
-    int32_t post_rvnd_avg_cost;
-    int32_t final_solution_avg_cost;
+    int32_t constructive_heuristic_avg_cost = 0;
+    int32_t rvnd_avg_cost = 0;
+    int32_t metaheuristic_solution_avg_cost = 0;
 
     std::vector<size_t> best_solution_sequence;
     int32_t best_solution_cost;
 
     // Time tracking info
-    std::chrono::duration<double> avg_elapsed_time;
+    std::chrono::duration<double> constructive_heuristic_avg_elapsed_time;
+    std::chrono::duration<double> rvnd_avg_elapsed_time;
+    std::chrono::duration<double> metaheuristic_avg_elapsed_time;
 
     // Member function to calculate the gap
-    double CalculateGap(int32_t optimal_value) {
-        if (optimal_value == 0) return 0.0; // Avoid division by zero
-        return static_cast<double>((final_solution_avg_cost - optimal_value) / optimal_value) * 100.0;
+    double CalculateGap(int32_t input_cost, int32_t optimal_cost) {
+        if (optimal_cost == 0) return 0.0; // Avoid division by zero
+        return static_cast<double>((input_cost - optimal_cost) / optimal_cost) * 100.0;
     }
 
     // Print benchmark results
-    void printResults(std::string instance_name, int32_t optimal_value) {
+    void printResults(std::string instance_name, int32_t optimal_cost) {
         std::cout << "Instance: " << instance_name << '\n';
-        std::cout << "Optimal Cost: " << optimal_value << '\n';
+        std::cout << "Optimal Cost: " << optimal_cost << '\n';
+        std::cout << "--------------------------Constructive Heuristic-----------------------------\n";
         std::cout << "Constructive Heuristic Avg Cost: " << constructive_heuristic_avg_cost << '\n';
-        std::cout << "Post-RVND Avg Cost: " << post_rvnd_avg_cost << '\n';
-        std::cout << "Final Solution Avg Cost: " << final_solution_avg_cost << '\n';
-        std::cout << "Average Elapsed Time: " << avg_elapsed_time.count() << " s" << '\n';
-        std::cout << "GAP: " << CalculateGap(optimal_value) << '\n';
-        std::cout << "============================================================================\n";
+        std::cout << "Constructive Heuristic Avg Time: " << constructive_heuristic_avg_elapsed_time.count() << '\n';
+        std::cout << "Constructive Heuristic GAP" << CalculateGap(constructive_heuristic_avg_cost, optimal_cost) << '\n';
+        std::cout << "----------------------------------RVND---------------------------------------\n";
+        std::cout << "RVND Avg Cost: " << rvnd_avg_cost << '\n';
+        std::cout << "RVND Avg Time: " << rvnd_avg_elapsed_time.count() << '\n';
+        std::cout << "RVND GAP: " << CalculateGap(rvnd_avg_cost, optimal_cost);
+        std::cout << "-----------------------------Metaheuristic-----------------------------------\n";
+        std::cout << "Metaheuristic Avg Cost: " << metaheuristic_solution_avg_cost << '\n';
+        std::cout << "Metahueristic Avg Time: " << metaheuristic_avg_elapsed_time.count() << " s" << '\n';
+        std::cout << "Metaheuristic GAP: " << CalculateGap(metaheuristic_solution_avg_cost, optimal_cost) << '\n';
+        std::cout << "--------------------------Best Solution Found--------------------------------\n";
+        std::cout << "Best Solution Cost: " << best_solution_cost << '\n';
         std::cout << "Best Solution Sequence: ";
 
         for (size_t element : best_solution_sequence) {
@@ -40,8 +50,6 @@ struct benchmarker_t {
         }
 
         std::cout << '\n';
-        std::cout << "Best Solution Cost: " << best_solution_cost << '\n';
-        std::cout << "============================================================================\n";
     }
 };
 
